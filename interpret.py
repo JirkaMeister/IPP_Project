4f39ca56dee35a2bc2c15287521625dd0d9713fd
Jiri Charamza <jicharamza@seznam.cz>
2023-04-14 23:00:01 +0200

    Add classes to store instructions

--- interpret.py
+++ interpret.py
@@ -8,6 +8,52 @@ import argparse
 import xml.etree.ElementTree as XML
 import sys
 
+class Program:
+    def __init__(self):
+        self.order = 0
+        self.instructions = []
+    
+    def __repr__(self):
+        return str("Program:" + str(self.instructions))
+
+    def getInstruction(self): 
+        instruction = self.instructions[self.order]
+        self.order += 1
+        return instruction
+
+    def addInstruction(self, instruction):
+        self.instructions.append(instruction)
+
+
+class Instruction:
+    def __init__(self, order, opcode):
+        self.order = order
+        self.opcode = opcode
+        self.args = []
+
+    def __repr__(self):
+        return str("\n  Instruction: " + self.opcode + str(self.args))
+
+    def addArg(self, arg):
+        self.args.append(arg)
+
+
+class Argument:
+    def __init__(self, argumentType, value):
+        self.type = argumentType
+        self.value = value
+
+    def __repr__(self):
+        if self.value:
+            return str("\n    Arg: type=" + self.type + "  value=" + self.value)
+        else:
+            return str("\n    Arg: " + self.type)
+
+
+class Frame:
+    pass
+
+
 def exitError(errorCode):
     match errorCode:
         case 10:
@@ -40,8 +86,10 @@ def exitError(errorCode):
 parser = argparse.ArgumentParser()
 parser.add_argument("--source", help="Source file", required=True)
 parser.add_argument("--input", help="Input file", required=True)
-args = vars(parser.parse_args())
-#TODO: change exit code to 10
+try:
+    args = vars(parser.parse_args())
+except:
+    exitError(10)
 
 # Open source file
 try:
@@ -55,27 +103,46 @@ try:
 except:
     exitError(11)
 
-# Parse xml
+# Check XML structure ans save instructions and arguments
+program = Program()
+
 try:
     xml = XML.parse(sourceFile).getroot()
     if xml.tag != "program" and xml.attrib["language"] != "IPPcode21":
-        raise Exception("ParseError")
-    for instruction in xml:
-        if instruction.tag != "instruction":
-            raise Exception("ParseError")
-        if "order" not in instruction.attrib or "opcode" not in instruction.attrib:
-            raise Exception("ParseError")
-        for arg in instruction:
-            if arg.tag != "arg1" and arg.tag != "arg2" and arg.tag != "arg3":
-                raise Exception("ParseError")
-            if "type" not in arg.attrib:
-                raise Exception("ParseError")
+        raise Exception("XML structure")
+    for instrXml in xml:
+
+        # Check instruction structure
+        if instrXml.tag != "instruction":
+            raise Exception("XML structure")
+        if "order" not in instrXml.attrib or "opcode" not in instrXml.attrib:
+            raise Exception("XML structure")
+        
+        # Create and save instruction
+        instruction = Instruction(instrXml.attrib["order"], instrXml.attrib["opcode"])
+        program.addInstruction(instruction)
+        
+        for argXml in instrXml:
+
+            # Check arguments structure
+            if not re.match(r'arg[1-3]', argXml.tag):
+                raise Exception("XML structure")
+            if "type" not in argXml.attrib:
+                raise Exception("XML structure")
+            
+            # Create and save argument
+            argument = Argument(argXml.attrib["type"], argXml.text)
+            instruction.addArg(argument)
+
 except Exception as exception:
-    if exception.args[0] == "ParseError":
+    if exception.args[0] == "XML structure":
+        print("huh")
         exitError(32)
     else:
         exitError(31)
 
+# Print instructions and arguments
+print(re.sub(r'[,\]\[]', '', program.__repr__()), file = sys.stderr)
 
 
 # Close files
