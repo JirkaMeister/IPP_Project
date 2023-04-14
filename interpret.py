0782d56d1cd42b71a9d9291fd34afd6e6ab6a51a
Jiri Charamza <jicharamza@seznam.cz>
2023-04-14 19:48:37 +0200

    Add file handling

--- interpret.py
+++ interpret.py
@@ -12,4 +12,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--source", help="Source file", required=True)
 parser.add_argument("--input", help="Input file", required=True)
 args = vars(parser.parse_args())
+#TODO: change exit code to 10
+
+# Open source file
+try:
+    sourceFile = open(args["source"], "r")
+except:
+    print("Error: Unable to open source file")
+    exit(11)
+
+# Open input file
+try:
+    inputFile = open(args["input"], "r")
+except:
+    print("Error: Unable to open input file")
+    exit(11)
+
 
